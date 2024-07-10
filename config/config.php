a24bf59171602d2ec99baa8a8fb3e430283015d2
kingsleyudenewu <kingzpacking@gmail.com>
2024-07-10 10:09:20 +0100

    Restructure payment router config file

--- config/config.php
+++ config/config.php
@@ -3,19 +3,26 @@
 /*
  * You can place your custom package configuration in here.
  */
+
+
 return [
     'processors' => [
-        'flutterwave' => [
-            'priority' => 1, // Lower value indicates higher priority
-            'currencies' => ['NGN'], // Supported currencies
-            'min_amount' => 100, // Minimum transaction amount
-            'class' => \App\Services\FlutterwavePaymentClient::class,
-        ],
         'paystack' => [
+            'name' => 'paystack',
+            'transaction_cost' => 1.5,
+            'priority' => 1, // Lower value indicates higher priority
+            'currencies' => ['NGN', 'USD', 'ZAR'], // Supported currencies
+            'status' => 'Active',
+            'class' => \Blinqpay\PaymentRouter\Clients\Paystack::class,
+        ],
+        'flutterwave' => [
+            'name' => 'flutterwave',
+            'transaction_cost' => 1.4,
             'priority' => 2, // Lower value indicates higher priority
-            'currencies' => ['NGN'], // Supported currencies
+            'currencies' => ['NGN', 'USD', 'CAD'], // Supported currencies
             'min_amount' => 100, // Minimum transaction amount
-            'class' => \App\Services\PaystackPaymentClient::class,
+            'status' => 'Active',
+            'class' => \Blinqpay\PaymentRouter\Clients\Flutterwave::class,
         ],
     ],
 ];
